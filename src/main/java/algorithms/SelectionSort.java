d213c33353c3b8b0d1fd0066c79f27cac4f1bfa1
belousov <belousov.nikolai@gmail.com>
2020-04-22 01:36:50 +0300

    Added SelectionSort solution

---
+++ src/main/java/algorithms/SelectionSort.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                swap(arr, i, minIndex);
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
